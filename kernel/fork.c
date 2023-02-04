4b328a0f9d000ec62eecb1d0418ec77e3294d0bd
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2023-02-05 02:36:38 +0600

    kernel/fork: Boost CPU based on Kprofiles selection
    - Boost for 50 ms when balance mode is active.
    - Boost for 100 ms when performance mode is active.
    - Upon selecting 0 for Kprofiles, it will default to using balance
      mode's configuration.
    - Battery mode's configuration is avoided since __devfreq_boost_kick_max()
      returns if battery set is set resulting in making itself a dummy function
      for the function call here.
    - Boost for 30 ms as fallback if CONFIG_KPROFILES is not enabled.
    
    Change-Id: d06633d5c980e695343d02676364907603a16e7d
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -2089,6 +2089,8 @@ struct task_struct *fork_idle(int cpu)
 	return task;
 }
 
+extern int kp_active_mode(void);
+
 /*
  *  Ok, this is the main fork-routine.
  *
@@ -2105,12 +2107,31 @@ long _do_fork(unsigned long clone_flags,
 	struct task_struct *p;
 	int trace = 0;
 	long nr;
+	unsigned int period = 30;
+
+	switch (kp_active_mode()) {
+	case 0: /* Use balance mode's boost period */
+	case 2:
+		/* Boost for 50 ms when balance mode is active */
+		period = 50;
+		break;
+	case 3:
+		/* Boost for 100 ms when performance mode is active */
+		period = 100;
+		break;
+	}
 
 	/* Boost CPU to the max for 50 ms when userspace launches an app */
 	if (task_is_zygote(current)) {
 		cpu_input_boost_kick_max(50);
 		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 50);
 	}
+
+#ifdef CONFIG_CPU_INPUT_BOOST
+	/* Boost CPU to the max when userspace launches an app */
+	if (task_is_zygote(current))
+		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, period);
+#endif
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When
